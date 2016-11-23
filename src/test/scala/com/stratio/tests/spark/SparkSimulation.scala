3282d55857380e91534fbcf4c9f7e1eeecf6b854
eruizgar <eruiz@stratio.com>
2016-11-23 18:03:30 +0100

    Fix spark simulation test.

--- src/test/scala/com/stratio/tests/spark/SparkSimulation.scala
+++ src/test/scala/com/stratio/tests/spark/SparkSimulation.scala
@@ -1,5 +1,6 @@
 package com.stratio.tests.spark
 
+import com.stratio.tests.commons.spark._
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef._
@@ -7,26 +8,16 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 import scala.sys.process._
 
-class SparkSimulation extends Simulation {
+class SparkSimulation extends Simulation with Headers {
 
-
-  val scn = scenario("Spark stability")
-    .exec(
-      SparkSimulation.createJob).exitHereIfFailed.pause(50)
-    .exec(
-      SparkSimulation.getStatus
-  ).exitHereIfFailed
-
-  setUp(scn.inject(rampUsers(2) over (120 seconds))).maxDuration(120 seconds)
-}
-
-object SparkSimulation extends Headers {
+  val ramp_users = Integer.parseInt(System.getProperty("RAMP_USERS", "4"))
+  val test_duration = Integer.parseInt(System.getProperty("TEST_DURATION", "120"))
 
   val dcos_user = System.getProperty("DCOS_USER", "admin@demo.stratio.com")
   val remote_user = System.getProperty("REMOTE_USER", "root")
   val remote_password = System.getProperty("REMOTE_PASSWORD", "stratio")
   val master_mesos = System.getProperty("MASTER_MESOS", "10.200.0.152")
-  val serviceName = System.getProperty("SERVICE_NAME", "spark")
+  val serviceName = System.getProperty("SERVICE_NAME", "spark1")
 
   val dcos_secret = s"""sshpass -p $remote_password ssh -t -T -o StrictHostKeyChecking=no $remote_user@$master_mesos cat /var/lib/dcos/dcos-oauth/auth-token-secret""" !!
   val token_pre = s"""java  -jar src/test/resources/dcosTokenGenerator-0.1.0-SNAPSHOT-jar-with-dependencies.jar $dcos_secret $dcos_user""" !!
@@ -34,54 +25,43 @@ object SparkSimulation extends Headers {
 
   val create =s"""http://$master_mesos/service/$serviceName/v1/submissions/create"""
   val status =s"""http://$master_mesos/service/$serviceName/v1/submissions/status/"""
-  val delete =s"""http://$master_mesos/service/$serviceName/v1/submissions/delete"""
-
-  val json =
-    """{
-    "action": "CreateSubmissionRequest",
-    "appArgs": ["", "10"],
-    "appResource": "https://s3-eu-west-1.amazonaws.com/enablers/spark-examples-1.6.1-hadoop2.4.0.jar",
-    "clientSparkVersion": "1.6.2",
-    "environmentVariables": {
-      "SPARK_SCALA_VERSION": "2.10",
-      "SPARK_JAVA_OPTS": "-Dspark.mesos.executor.docker.image=qa.stratio.com/stratio/stratio-spark:1.0.1-1.6.2",
-      "SPARK_HOME": "/root/.dcos/spark/dist/spark-1.6.2"
-    },
-    "mainClass": "org.apache.spark.examples.JavaSparkPi",
-    "sparkProperties": {
-      "spark.jars": "https://s3-eu-west-1.amazonaws.com/enablers/spark-examples-1.6.1-hadoop2.4.0.jar",
-      "spark.ssl.noCertVerification": "true",
-      "spark.driver.supervise": "false",
-      "spark.app.name": "org.apache.spark.examples.JavaSparkPi",
-      "spark.mesos.executor.docker.image": "qa.stratio.com/stratio/stratio-spark:1.0.1-1.6.2",
-      "spark.submit.deployMode": "cluster"
-    }
-  }"""
+  val delete =s"""http://$master_mesos/service/$serviceName/v1/submissions/kill/"""
 
   val headers = Map(contentType -> contentTypeValue, cookie -> (auth + token))
 
 
-  def createJob = {
-    http("Create job")
-      .post(create)
-      .headers(headers)
-      .body(StringBody(json))
-      .check(jsonPath("$.submissionId").saveAs("submissionId"))
+  val createJob = exec(http("Create job")
+    .post(create)
+    .headers(headers)
+    .body(StringBody(json))
+    .check(jsonPath("$.submissionId").saveAs("submissionId")))
 
+  val getStatus = exec(http("Status job").get(status + "${submissionId}")
+    .headers(headers).check(jsonPath("$.driverState").saveAs("driverState")))
+
+  val checkStatus =
+    exec(
+      http("Check job").get(status + "${submissionId}")
+    .headers(headers).check(checkIf(session =>
+      session.attributes("driverState").asInstanceOf[String] == "FINISHED"){
+      jsonPath("$.driverState").is("FINISHED")
+    }))
+
+
+  val scn = scenario("Spark stability")
+        .exec(createJob).exec(getStatus).asLongAs(session =>
+            session.attributes("driverState").asInstanceOf[String] != "FINISHED"){
+        exec(getStatus).pause(5)
+          .doIf(session =>
+            session.attributes("driverState").asInstanceOf[String] == "FINISHED"){
+            exec(checkStatus)
+          }
   }
-  def getStatus = {
-    http("Status job").get(status + "${submissionId}")
-      .headers(headers)
-      .check(jsonPath("$.driverState").saveAs("driverState"))
-      .check(jsonPath("$.driverState").is("FINISHED"))
-  }
+
+
+  setUp(scn.inject(rampUsers(ramp_users) over (100 seconds))).maxDuration(test_duration seconds)
+
 }
 
-trait Headers {
-  val contentTypeValue: String = "application/json; charset=utf-8"
-  val contentType = "Content-Type"
-  val cookie = "Cookie"
-  val auth="dcos-acs-auth-cookie="
-}
 
 
